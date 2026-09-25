539547b58253ea0d78765577246163205132d838
Crocod1llol <Crocod1llol@users.noreply.github.com>
2026-09-25 18:55:27 +0300

    made function work and now 'pwd' return current working dir

--- src/commands.zig
+++ src/commands.zig
@@ -1,11 +1,10 @@
 ///the file that holds built in commands and the func that executes non-built in commands found in the file system
-
 const std = @import("std");
 
+const main = @import("main.zig");
+
 ///executes a builtin command's function, or if its short, the command itself
-///return a u8 of the state that the command sent 
-///0 - completed with success, 1 - exited with failure, 255 - it requested to exit
-pub fn exec_buildin(command: []const u8, init: std.process.Init) !u8 {
+pub fn exec_buildin(command: []const u8, init: std.process.Init) !void {
 
     //make the output file interface for good measures
     var out_buf: [512]u8 = undefined;
@@ -15,35 +14,31 @@ pub fn exec_buildin(command: []const u8, init: std.process.Init) !u8 {
     //fuck this
     if (std.mem.eql(u8, command, "")) {
 
-        return 0;
     } else if (std.mem.eql(u8, command, "exit")) {
 
-        //req to exit
-        return 255;
+        std.process.exit(0);
     } else if (std.mem.eql(u8, command, "hack")) {
 
         //TEMP: it will just print this
         try out_file.interface.print("hack\n", .{});
         try out_file.interface.flush();
 
-        return 0;
     } else if (std.mem.eql(u8, command, "clear")) {
 
         //insert funny chars to clear the screen
         try out_file.interface.print("{c}[2J{c}[1;1H", .{27, 27});
         try out_file.interface.flush();
 
-        return 0;
 
     } else if (std.mem.eql(u8, command, "cd")) {
 
-        return 0;
+
+    } else if (std.mem.eql(u8, command, "pwd")) {
+
+        _ = try main.print_cwd(init, true); 
     } else {
-
-        std.log.err("Unknown built-in command: {s}", .{command});
-
-        //if nothing is matching, return 1
-        return 1;
+        //it completed with success ok bro?
+        std.log.info("Command completed succesfully!", .{});
     }
 }
 
--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,63 @@ const std = @import("std");
 //import files
 const commands = @import("commands.zig");
 
+//nothing on this planet works 
+//dfughkdfgksdlfhlgjdsfhlksghdlkfj sbh7g98rno,p9mvtc bm.o9<F7><F4>
+//ce <F4>cghu\y6gbnhu<F7><F6>8<F5><F8><F4>57gnuo9-0<F8>=<F9><F7><F6>gthujo9<F6>8<F7><F8>yujoi<F10>\9<F7>6b vbhjytvmnuyc
+//nj7thvnhjiohtvmloytghb
+var debug_allocator = std.heap.DebugAllocator(.{}){};
+
+const gpa = debug_allocator.allocator();
+
+//const u32_ptr = try gpa.create(u32);
+//_ = u32_ptr; // silences unused variable error
+
+//global constants
+const MAX_FILEPATH = 4096;
+
+///a func to print or return the current working dir (cwd)
+pub fn print_cwd(init: std.process.Init, will_print: bool) ![]u8 {
+
+    //setup stdout buf is required
+    var out_buf: [4096]u8 = undefined;
+    var output_file = std.Io.File.writer(std.Io.File.stdout(), init.io, &out_buf);
+
+    //alloc memory so that it lasts long enough so i can return it
+    const cwd_buf: *[MAX_FILEPATH]u8 = try gpa.create([MAX_FILEPATH]u8);
+
+    const status = std.os.linux.getcwd(cwd_buf, MAX_FILEPATH);
+
+    //check if it failed
+    if (status == 0) {
+
+        std.log.err("getcwd linux sys call failed", .{});
+    }
+
+    //apperently status returns how many chars does a filepath have, so cool ig
+    const trimmed_cwd_buf = cwd_buf.*[0..status];
+
+    if (will_print) {
+
+        try output_file.interface.print("{s}\n", .{trimmed_cwd_buf});
+        try output_file.interface.flush();
+
+        return "";
+    } else {
+
+        return trimmed_cwd_buf;
+    }
+
+    //and because of that problem, i will free it later
+    defer gpa.destroy(cwd_buf);
+
+    return "";
+}
+
 pub fn main(init: std.process.Init) !void {
+
+    //fun fact: i have to defer something
+    defer std.debug.assert(debug_allocator.deinit() == .ok);
+
     //setup stdout file handler to print
     var out_buf: [512]u8 = undefined;
     var output_file = std.Io.File.writer(std.Io.File.stdout(), init.io, &out_buf);
@@ -30,9 +86,8 @@ pub fn main(init: std.process.Init) !void {
         //make zig take all the bytes except the unused ones, so we can compare strings
         const trimmed_result = result orelse break;
 
-        if (try commands.exec_buildin(trimmed_result, init) == 255) {
+        //check if the shell has been asked to exit
+        try commands.exec_buildin(trimmed_result, init);
 
-            break;
-        }
     }
 }
